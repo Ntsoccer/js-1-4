6dc755f3d45eb00ab3207de24e62c5b66cfce42d
Ntsoccer <2460socceronly0512@gmail.com>
2021-01-22 03:19:29 +0900

    second commit

--- index.js
+++ index.js
@@ -4,34 +4,35 @@ const list = {
 }
 const displayText = `現在持っているタスク一覧\n`;
 
-function listDisplay() {
+function displayList() {
   console.log(displayText);
   for (let i = 0; i < list.contents.length; i++) {
-    console.log(`${i}:[内容]${list.contents[i]}、[ジャンル]${list.genres[i]}`);
+    console.log(`${i} : [内容]${list.contents[i]}、[ジャンル]${list.genres[i]}`);
   }
 }
-listDisplay()
+displayList()
 
-function listAddTask(task) {
+function addTask(task) {
   list.contents.push(task);
 }
 
-function listAddGenre(genre) {
+function AddGenre(genre) {
   list.genres.push(genre);
 }
 
 let i = 2;
-listDisplay();
+// displayList();
 while (true) {
+  displayList();
   i++;
   const task = prompt('タスクを入力してください')
-  listAddTask(task);
+  addTask(task);
   const genre = prompt('ジャンルを入力してください')
-  listAddGenre(genre);
-  console.log(`${i}:[内容]${list.contents[i]}、[ジャンル]${list.genres[i]}`);
+  AddGenre(genre);
+  console.log(`${i} : [内容]${list.contents[i]}、[ジャンル]${list.genres[i]}`);
   const text = prompt("「確認,追加,削除,終了」の4つのいずれかを入力してください");
   if (text === '確認') {
-    listDisplay();
+    displayList();
   } else if (text === '追加') {
     list.contents.push(text);
     list.genres.push(text);
